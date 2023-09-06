0f2fe488250694aa2db7f609dac99233b9702725
ljw96 <dlwodnjs2669@gmail.com>
2023-09-06 14:57:20 +0900

    파이썬 1일차 기본

---
+++ 기본.py
@@ -0,0 +1,57 @@
+print("Hello, world")
+print("Hello, world")
+print(100)
+print(33.33)
+print(100+23)
+
+# 여기는 주석입니다.
+"""
+여기는 범위 주석 구간 입니다.
+ㅔㅁ로엘메ㅗㄹ메오낼메롱~~
+"""
+
+# 변수를 선언하고 값을 대입
+num = 100 # 데이터형은 값이 대입되는 순간에 결정 남
+print(num)
+num = 1235
+print(num)
+
+# 변수 활용
+name = "이재원"
+email = "dlwodnjs2669@gmail.com"
+age = 28
+addr = '서울시 강남구 역삼동 KH정보교육원'
+
+print(f"""
+이름 : {name}
+이메일 : {email}
+나이 : {age}
+주소 : {addr}
+""")
+
+# 파이썬은 불리언 값이 대문자로 시작
+isAdult = True
+
+if isAdult :
+    print("나는 성인 입니다.")
+else :
+    print("나는 성인이 아닙니다")
+
+
+"""
+    작성자 : 이재원
+    수정날짜 : 2023.09.06
+"""
+
+'''
+작성자 : 이재원
+    수정날짜 : 2023.09.06
+'''
+
+print([1, 2, 3])  # 리스트 출력
+
+
+
+
+
+
