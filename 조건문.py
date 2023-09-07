337ae870ca9e91f8a922af1c04d2f80dec7525dc
ljw96 <dlwodnjs2669@gmail.com>
2023-09-07 12:50:26 +0900

    파이썬 2일차 조건문

---
+++ 조건문.py
@@ -0,0 +1,28 @@
+# 제어문이란 : 조건문과 반복문을 의미하며 순차적인 흐름을 제어하는 목적으로 사용 된다.
+
+# n = int(input("정수를 입력 하세요 : "))
+# if n > 100:
+#     print(f"{n}은 100보다 큽니다.")
+# elif n < 100:
+#     print(f"{n}은 100보다 작습니다.")
+# else:
+#     print("100과 같습니다.")
+
+# 조건문에서 문자열 비교
+
+while True:
+    season = input("현재 계절을 입력하세요 : ")
+    if season == "spring":
+        print("봄이 왔어요")
+        break
+    elif season == "summer":
+        print("여름이 왔어요")
+        break
+    elif season == "fall" or season == "autumn":
+        print("가을이 왔어요")
+        break
+    elif season == "winter":
+        print("추운 겨울 입니다.")
+        break
+    else:
+        print("계절을 잘못 입력 하셨습니다.")
