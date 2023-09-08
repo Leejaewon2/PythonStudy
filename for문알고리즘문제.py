ec0ad1023491552c49762391e60c8f80ad6b66ea
ljw96 <dlwodnjs2669@gmail.com>
2023-09-08 20:49:10 +0900

    파이썬 3일차 for문 알고리즘문제 연습

---
+++ for문알고리즘문제.py
@@ -0,0 +1,28 @@
+# 구구단 만들기
+# for i in range(2, 10):
+#     for j in range(1, 10):
+#         print(f"{i}x{j} : {i * j}")
+#     print()
+
+# A+B-3
+
+# n개의 합 구하기
+# n = int(input("정수 : "))
+# sum = 0
+# for i in range(1, n + 1):
+#     sum += i
+#     print(sum)
+
+# 영수증
+# x = int(input("총 금액 : "))
+# n = int(input("구매한 물건이 개수 : "))
+# sum = 0
+# for i in range (n):
+#     a, b = map(int, input("물건 금액, 개수 : ").split())
+#     sum = a * b
+#     x = x - sum
+# if sum == x:
+#     print("yes")
+# else:
+#     print("No")
+
